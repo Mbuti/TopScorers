5464e7d377fac6a5db4a462233e2c90b87890fe5
Mbuti <buthelezi.mbuti@gmail.com>
2023-03-21 13:03:47 +0200

    Update TopScorers.java

--- TopScores/src/TopScorers.java
+++ TopScores/src/TopScorers.java
@@ -49,7 +49,7 @@ public class TopScorers {
             }
         }
 
-        Collections.sort(Topscorers, Comparator.comparing(Scorer::getScore));
+        Collections.sort(Topscorers, Comparator.comparing(Scorer::getName));
         return Topscorers;
     }
 
